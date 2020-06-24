a19a7d6f8c2be2e30e8b3aea5e1af211bb9f47f4
gabriel <llanesgabrielg@gmail.com>
2020-06-24 15:06:21 -0300

    commit punto2-calculadora

--- tp6/tp6/Program.cs
+++ tp6/tp6/Program.cs
@@ -11,42 +11,152 @@ namespace tp6
          *  - la posicion inicial
          *  - la longitud a devolver
          */
-        public static string partCad(string cadena, int indInicial, int posicion)
+
+        //    public static string partCad(string cadena, int indInicial, int posicion)
+        //    {
+        //        string result = cadena.Substring(indInicial, posicion);
+        //        return result;
+        //    }
+
+        //static void Main(string[] args)
+        //    {
+        //        string numero,invertida="";
+        //        int posicion,numTOentero;
+
+        //        Console.Write("ingresar numero a invertir: ");
+        //        numero = Console.ReadLine(); // recibe una cadena
+        //        numTOentero = Convert.ToInt32(numero); // casteo a numTOentero para hacer el control
+        //        posicion = numero.Length; // tamaño de la cadena 
+
+        //        if (numTOentero > 0)
+        //        {
+        //            while (posicion > 0)
+        //            {
+        //                // en cada ciclo del bucle obtenemos un caracter empezando por el
+        //                // final para invertir la cadena.
+        //                invertida = invertida + partCad(numero, posicion - 1, 1);
+        //                posicion = posicion - 1;
+        //            }
+
+        //            Console.WriteLine("La numero invertida es: " + invertida);
+
+        //        }
+        //        else
+        //        {
+        //            Console.WriteLine("no se puede hacer la conversion de numeros menores o iguales a cero");
+        //        }
+
+
+        //    }
+
+        public static void menu()
         {
-            string result = cadena.Substring(indInicial, posicion);
-            return result;
+            Console.WriteLine("\n-------menu calculadora---------\n");
+            Console.WriteLine("\n_SUMA_(1)");
+            Console.WriteLine("\n_RESTA_(2)");
+            Console.WriteLine("\n_MULTIPLICACION_(3)");
+            Console.WriteLine("\n_DIVICION_(4)");
+            //Console.WriteLine("\n_SALIR DEL PROGRAMA_(5)");
+            Console.WriteLine("\n-------------------\n");
         }
- 
-        static void Main(string[] args)
+        public static int salir(char opcionSalir)
         {
-            string numero,invertida="";
-            int posicion,numTOentero;
- 
-            Console.Write("ingresar numero a invertir: ");
-            numero = Console.ReadLine(); // recibe una cadena
-            numTOentero = Convert.ToInt32(numero); // casteo a numTOentero para hacer el control
-            posicion = numero.Length; // tamaño de la cadena 
-            
-            if (numTOentero > 0)
+            if (opcionSalir == 'n' || opcionSalir == 'N')
             {
-                while (posicion > 0)
-                {
-                    // en cada ciclo del bucle obtenemos un caracter empezando por el
-                    // final para invertir la cadena.
-                    invertida = invertida + partCad(numero, posicion - 1, 1);
-                    posicion = posicion - 1;
-                }
-
-                Console.WriteLine("La numero invertida es: " + invertida);
-
+                return 5;
             }
             else
             {
-                Console.WriteLine("no se puede hacer la conversion de numeros menores o iguales a cero");
+                return 0;
             }
-            
-            
         }
+        static void Main(string[] args)
+        {
+            int opcion, num1, num2, multiplicacion, division;
+            char opcionChar;
+            do
+            {
+                menu();
+                Console.WriteLine("\n\n SELECCIONAR UNA OPCION : ");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                // opcionSalir = Convert.ToChar(opcion); // 
+
+                switch (opcion)
+                {
+                    case 1:
+                        Console.Clear(); // limpiar pantalla
+                        Console.WriteLine("ingrese dos numeros enteros  : ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("la suma es : " + (num1 + num2));
+                        Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+                        opcionChar = Convert.ToChar(Console.ReadLine());
+                        opcion = salir(opcionChar);
+                        Console.Clear();// limpiar pantalla
+
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("ingrese dos numeros enteros  : ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("la resta es : " + (num1 - num2));
+                        Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+                        opcionChar = Convert.ToChar(Console.ReadLine());
+                        opcion = salir(opcionChar);
+                        Console.Clear();
+
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("ingrese dos numeros enteros : ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        multiplicacion = num1 * num2;
+                        Console.WriteLine("la multiplicacion  es : " + (multiplicacion));
+                        Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+                        opcionChar = Convert.ToChar(Console.ReadLine());
+                        opcion = salir(opcionChar);
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("ingrese dos numeros enteros : ");
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                        if (num2 != 0)
+                        {
+                            division = num1 / num2;
+                            Console.WriteLine("la division es : " + (division));
+                            Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+                            opcionChar = Convert.ToChar(Console.ReadLine());
+                            opcion = salir(opcionChar);
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.WriteLine("error, no se puede dividir por cero");
+                            Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+                            opcionChar = Convert.ToChar(Console.ReadLine());
+                            opcion = salir(opcionChar);
+                            Console.Clear();
+                        }
+                        break;
+                    default:
+                        if (opcion != 5)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("opcion incorrecta, vuelva a ingresar");
+                        }
+
+                        break;
+                }
+
+            } while (opcion != 5);
+            Console.WriteLine("chau chau adios!! ");
+        }
+
+
     }
 }
 
