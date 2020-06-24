cc30e37465a4ad6159b6cebfa9f5d72f08af6b10
gabriel <llanesgabrielg@gmail.com>
2020-06-24 17:50:47 -0300

    punto 3

--- tp6/tp6/Program.cs
+++ tp6/tp6/Program.cs
@@ -49,113 +49,141 @@ namespace tp6
 
         //    }
 
-        public static void menu()
+        //public static void menu()
+        //{
+        //    Console.WriteLine("\n-------menu calculadora---------\n");
+        //    Console.WriteLine("\n_SUMA_(1)");
+        //    Console.WriteLine("\n_RESTA_(2)");
+        //    Console.WriteLine("\n_MULTIPLICACION_(3)");
+        //    Console.WriteLine("\n_DIVICION_(4)");
+        //    //Console.WriteLine("\n_SALIR DEL PROGRAMA_(5)");
+        //    Console.WriteLine("\n-------------------\n");
+        //}
+        //public static int salir(char opcionSalir)
+        //{
+        //    if (opcionSalir == 'n' || opcionSalir == 'N')
+        //    {
+        //        return 5;
+        //    }
+        //    else
+        //    {
+        //        return 0;
+        //    }
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int opcion, num1, num2, multiplicacion, division;
+        //    char opcionChar;
+        //    do
+        //    {
+        //        menu();
+        //        Console.WriteLine("\n\n SELECCIONAR UNA OPCION : ");
+        //        opcion = Convert.ToInt32(Console.ReadLine());
+        //        // opcionSalir = Convert.ToChar(opcion); // 
+
+        //        switch (opcion)
+        //        {
+        //            case 1:
+        //                Console.Clear(); // limpiar pantalla
+        //                Console.WriteLine("ingrese dos numeros enteros  : ");
+        //                num1 = Convert.ToInt32(Console.ReadLine());
+        //                num2 = Convert.ToInt32(Console.ReadLine());
+
+        //                Console.WriteLine("la suma es : " + (num1 + num2));
+        //                Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+        //                opcionChar = Convert.ToChar(Console.ReadLine());
+        //                opcion = salir(opcionChar);
+        //                Console.Clear();// limpiar pantalla
+
+        //                break;
+        //            case 2:
+        //                Console.Clear();
+        //                Console.WriteLine("ingrese dos numeros enteros  : ");
+        //                num1 = Convert.ToInt32(Console.ReadLine());
+        //                num2 = Convert.ToInt32(Console.ReadLine());
+        //                Console.WriteLine("la resta es : " + (num1 - num2));
+        //                Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+        //                opcionChar = Convert.ToChar(Console.ReadLine());
+        //                opcion = salir(opcionChar);
+        //                Console.Clear();
+
+        //                break;
+        //            case 3:
+        //                Console.Clear();
+        //                Console.WriteLine("ingrese dos numeros enteros : ");
+        //                num1 = Convert.ToInt32(Console.ReadLine());
+        //                num2 = Convert.ToInt32(Console.ReadLine());
+        //                multiplicacion = num1 * num2;
+        //                Console.WriteLine("la multiplicacion  es : " + (multiplicacion));
+        //                Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+        //                opcionChar = Convert.ToChar(Console.ReadLine());
+        //                opcion = salir(opcionChar);
+        //                Console.Clear();
+        //                break;
+        //            case 4:
+        //                Console.Clear();
+        //                Console.WriteLine("ingrese dos numeros enteros : ");
+        //                num1 = Convert.ToInt32(Console.ReadLine());
+        //                num2 = Convert.ToInt32(Console.ReadLine());
+        //                if (num2 != 0)
+        //                {
+        //                    division = num1 / num2;
+        //                    Console.WriteLine("la division es : " + (division));
+        //                    Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+        //                    opcionChar = Convert.ToChar(Console.ReadLine());
+        //                    opcion = salir(opcionChar);
+        //                    Console.Clear();
+        //                }
+        //                else
+        //                {
+        //                    Console.WriteLine("error, no se puede dividir por cero");
+        //                    Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
+        //                    opcionChar = Convert.ToChar(Console.ReadLine());
+        //                    opcion = salir(opcionChar);
+        //                    Console.Clear();
+        //                }
+        //                break;
+        //            default:
+        //                if (opcion != 5)
+        //                {
+        //                    Console.Clear();
+        //                    Console.WriteLine("opcion incorrecta, vuelva a ingresar");
+        //                }
+
+        //                break;
+        //        }
+
+        //    } while (opcion != 5);
+        //    Console.WriteLine("chau chau adios!! ");
+        //}
+
+        // punto 3
+        public static void Mostrar(float numero)
         {
-            Console.WriteLine("\n-------menu calculadora---------\n");
-            Console.WriteLine("\n_SUMA_(1)");
-            Console.WriteLine("\n_RESTA_(2)");
-            Console.WriteLine("\n_MULTIPLICACION_(3)");
-            Console.WriteLine("\n_DIVICION_(4)");
-            //Console.WriteLine("\n_SALIR DEL PROGRAMA_(5)");
-            Console.WriteLine("\n-------------------\n");
-        }
-        public static int salir(char opcionSalir)
-        {
-            if (opcionSalir == 'n' || opcionSalir == 'N')
-            {
-                return 5;
-            }
-            else
-            {
-                return 0;
-            }
+            int partInt;
+            Console.WriteLine("\nel valor absoluto es : " + (Math.Abs(numero)));
+
+            Console.WriteLine("\n_el cuadrado es : " + (Math.Pow(numero, 2)));
+            Console.WriteLine("\n_la raiz es : " + Math.Sqrt(numero));
+            Console.WriteLine("\n_el seno es : " + (Math.Sin(numero)));
+            Console.WriteLine("\n_el coseno es: " + (Math.Cos(numero)));
+            partInt = Convert.ToInt32(numero); // casteo a entero
+            Console.WriteLine("\n la parte entera es\n" + partInt); // muestro solo la parte entera
         }
         static void Main(string[] args)
         {
-            int opcion, num1, num2, multiplicacion, division;
-            char opcionChar;
-            do
-            {
-                menu();
-                Console.WriteLine("\n\n SELECCIONAR UNA OPCION : ");
-                opcion = Convert.ToInt32(Console.ReadLine());
-                // opcionSalir = Convert.ToChar(opcion); // 
+            float numero, numero2, numero3;
+            Console.WriteLine("\n ingresar un numero :\n ");
 
-                switch (opcion)
-                {
-                    case 1:
-                        Console.Clear(); // limpiar pantalla
-                        Console.WriteLine("ingrese dos numeros enteros  : ");
-                        num1 = Convert.ToInt32(Console.ReadLine());
-                        num2 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("la suma es : " + (num1 + num2));
-                        Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
-                        opcionChar = Convert.ToChar(Console.ReadLine());
-                        opcion = salir(opcionChar);
-                        Console.Clear();// limpiar pantalla
+            numero = Convert.ToSingle(Console.ReadLine());// hago ingreso y casteo a float 
+            Mostrar(numero); // funcion para mostrar todo lo pedido
+            Console.WriteLine("\n ingresar dos numeros nuevos :\n");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+            numero3 = Convert.ToSingle(Console.ReadLine());
 
-                        break;
-                    case 2:
-                        Console.Clear();
-                        Console.WriteLine("ingrese dos numeros enteros  : ");
-                        num1 = Convert.ToInt32(Console.ReadLine());
-                        num2 = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("la resta es : " + (num1 - num2));
-                        Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
-                        opcionChar = Convert.ToChar(Console.ReadLine());
-                        opcion = salir(opcionChar);
-                        Console.Clear();
-
-                        break;
-                    case 3:
-                        Console.Clear();
-                        Console.WriteLine("ingrese dos numeros enteros : ");
-                        num1 = Convert.ToInt32(Console.ReadLine());
-                        num2 = Convert.ToInt32(Console.ReadLine());
-                        multiplicacion = num1 * num2;
-                        Console.WriteLine("la multiplicacion  es : " + (multiplicacion));
-                        Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
-                        opcionChar = Convert.ToChar(Console.ReadLine());
-                        opcion = salir(opcionChar);
-                        Console.Clear();
-                        break;
-                    case 4:
-                        Console.Clear();
-                        Console.WriteLine("ingrese dos numeros enteros : ");
-                        num1 = Convert.ToInt32(Console.ReadLine());
-                        num2 = Convert.ToInt32(Console.ReadLine());
-                        if (num2 != 0)
-                        {
-                            division = num1 / num2;
-                            Console.WriteLine("la division es : " + (division));
-                            Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
-                            opcionChar = Convert.ToChar(Console.ReadLine());
-                            opcion = salir(opcionChar);
-                            Console.Clear();
-                        }
-                        else
-                        {
-                            Console.WriteLine("error, no se puede dividir por cero");
-                            Console.WriteLine("\ndesea continuar realizando calculos,  S/N : ");
-                            opcionChar = Convert.ToChar(Console.ReadLine());
-                            opcion = salir(opcionChar);
-                            Console.Clear();
-                        }
-                        break;
-                    default:
-                        if (opcion != 5)
-                        {
-                            Console.Clear();
-                            Console.WriteLine("opcion incorrecta, vuelva a ingresar");
-                        }
-
-                        break;
-                }
-
-            } while (opcion != 5);
-            Console.WriteLine("chau chau adios!! ");
+            Console.WriteLine("el mayor de los numeros ingresados es :" + (Math.Max(numero2, numero3)));
+            Console.WriteLine("el menor de los numeros ingresados es :" + (Math.Min(numero2, numero3)));
         }
-
 
     }
 }
